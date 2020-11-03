5160aef8c6eca00a12fafea484348349f2f7f8a2
stu124043 <stu124043@mail.uni-kiel.de>
2020-11-03 09:50:57 +0100

    stopped exception for wrong input. Now the program simply ignores wrong input

--- Form1.cs
+++ Form1.cs
@@ -39,37 +39,44 @@ namespace Perceptron_App
         {
             //generates the new dataset
             string amount = textBox1.Text;
-            List<ModelPoint> model = modelGenerator.GenerateNewSet(int.Parse(amount), radioButton2.Checked);
-            
-            //prints the given model on the console (enable for debugging)
-            //Console.WriteLine(modelGenerator.toString());
-
-            //TODO draw the dataset on the canvas
-            Graphics g = this.CreateGraphics();
-            g.Clear(Color.White);
-            Pen p = new Pen(Color.Black);
-            Pen blueP = new Pen(Color.Blue);
-            Pen redP = new Pen(Color.Red);
-
-            //draws the coordinate system
-            g.DrawLine(p, 25, 700, 25, 0);
-            g.DrawLine(p, 25, 700, 725, 700);
-
-            //draws the points 
-            List<ModelPoint> pointList = modelGenerator.points;
-            if (pointList.Count > 0)
+            try
             {
-                for (int i = 0; i < pointList.Count; ++i)
+                List<ModelPoint> model = modelGenerator.GenerateNewSet(int.Parse(amount), radioButton2.Checked);
+                
+                //prints the given model on the console (enable for debugging)
+                //Console.WriteLine(modelGenerator.toString());
+
+                //draws the dataset on the canvas
+                Graphics g = this.CreateGraphics();
+                g.Clear(Color.White);
+                Pen p = new Pen(Color.Black);
+                Pen blueP = new Pen(Color.Blue);
+                Pen redP = new Pen(Color.Red);
+
+                //draws the coordinate system
+                g.DrawLine(p, 25, 700, 25, 0);
+                g.DrawLine(p, 25, 700, 725, 700);
+
+                //draws the points 
+                List<ModelPoint> pointList = modelGenerator.points;
+                if (pointList.Count > 0)
                 {
-                    if (pointList[i].Color == Color.Blue)
+                    for (int i = 0; i < pointList.Count; ++i)
                     {
-                        g.DrawEllipse(blueP, 25 + (pointList[i].X * 7), 700 - (pointList[i].Y *7), 7, 7);
-                    }
-                    else
-                    {
-                        g.DrawEllipse(redP, 25 + (pointList[i].X * 7), 700 - (pointList[i].Y * 7), 7, 7);
+                        if (pointList[i].Color == Color.Blue)
+                        {
+                            g.DrawEllipse(blueP, 25 + (pointList[i].X * 7), 700 - (pointList[i].Y * 7), 7, 7);
+                        }
+                        else
+                        {
+                            g.DrawEllipse(redP, 25 + (pointList[i].X * 7), 700 - (pointList[i].Y * 7), 7, 7);
+                        }
                     }
                 }
+            }
+            catch (System.FormatException exception)
+            {
+                //do nothing
             }
         }
 
