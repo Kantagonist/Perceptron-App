1d7dd98ca5c91f355e929adc3d0a9ac32a552205
stu124043 <stu124043@mail.uni-kiel.de>
2020-11-04 11:21:43 +0100

    fixed the weights

--- Perceptron.cs
+++ Perceptron.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,33 +22,44 @@ namespace Perceptron_App
         public void train(List<ModelPoint> points, float errorThreshold)
         {
             //set configs
-            float[] initialWeights = new float[] { 0, 0.5f, 0.5f };
+            float[] weights = new float[] { 0, 0.5f, 0.5f };
             float learningRate = 0.1f;
-
-            //initialize the weight vectors with 0
-            Dictionary<ModelPoint, float[]> weights = new Dictionary<ModelPoint, float[]>();
-            for(int i = 0; i < points.Count; ++i)
-            {
-                weights.Add(points[i], initialWeights);
-            }
-
-            //initialize the threshold
+            float iterationError = 0.95f;
 
             //actual learning, but capped with a hard stop if iteration error can not be reached
             float[] y = new float[points.Count];
+            List<ModelPoint> misclassified = new List<ModelPoint>();
             int hardStop = 1000; //adjust at will
             for(int iteration = 0; iteration < hardStop; ++iteration)
             {
-                //calculate the output of the threshold function
+                //reset counters and arrays
                 int k = 0;
-                foreach (KeyValuePair<ModelPoint, float[]> entry in weights)
+                misclassified.Clear();
+                y = new float[points.Count];
+                
+                //calculate the output of the weighted graph and flag misclassified points 
+                foreach (ModelPoint entry in points)
                 {    
-                    y[k++] = entry.Value[0] + (entry.Key.X * entry.Value[1]) + (entry.Key.Y * entry.Value[2]);
+                    y[k] = weights[0] + (entry.X * weights[1]) + (entry.Y * weights[2]);
+                    if (y[k] > 0 && entry.Color == Color.Blue) //weights consider the entry to be red, but it is blue
+                    {
+                        misclassified.Add(entry);
+                    }
+                    else if (y[k] <= 0 && entry.Color == Color.Red) //weights consider the entry to be blue, but it is red
+                    {
+                        misclassified.Add(entry);
+                    }
+                    k++;
                 }
 
-                //calculate the iteration error, break if enough
-
                 //update the weights
+                foreach(ModelPoint entry in misclassified)
+                {
+                    float d =  entry.Color == Color.Red ? 1 : -1;
+                    weights[0] = weights[0] + (learningRate * d * 1);
+                    weights[1] = weights[1] + (learningRate * d * entry.X);
+                    weights[2] = weights[2] + (learningRate * d * entry.Y);
+                }
             }
         }
 
