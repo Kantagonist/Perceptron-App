75c45733f9d3557eb3c90db7bd63cf34149fca4c
stu124043 <stu124043@mail.uni-kiel.de>
2020-11-13 13:04:41 +0100

    i think the weights are wrong

--- Perceptron/Perceptron.cs
+++ Perceptron/Perceptron.cs
@@ -121,7 +121,6 @@ namespace Perceptron_App
         /// <returns></returns>
         private void getFunction()
         {
-            //float m = (-weights[0] / weights[2]) / (weights[0] / weights[1]);
             float xIntercept = -weights[0] / weights[2];
             float yIntercept = -weights[0] / weights[1];
             float m = yIntercept / xIntercept;
